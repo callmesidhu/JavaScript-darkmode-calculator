abd89f074f024f80b0bb2257d098746a314293ef
Sidharth <sidharthpunalur@gmail.com>
2022-05-14 02:50:31 -0700

    all operations added

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 var operation=0,operationValue=0;
-function equals() {
-    if(operation==1) {
-        operationValue=operationValue+parseInt(document.getElementById("output-screen").value);
-        document.getElementById("output-screen").value=operationValue;
-        operation=0;
-        operationValue=0;
-    }
-}
+
 function clearValue() {
     document.getElementById("output-screen").value="";
 }
@@ -33,4 +26,35 @@ function division() {
     operation=4;
     operationValue=parseInt(document.getElementById("output-screen").value);
     document.getElementById("output-screen").value="";
+}
+function equals() {
+    if(operation==1) {
+        operationValue=operationValue+parseInt(document.getElementById("output-screen").value);
+        document.getElementById("output-screen").value=operationValue;
+        operation=0;
+        operationValue=0;
+    }
+    else if(operation==2) {
+        operationValue=operationValue-parseInt(document.getElementById("output-screen").value);
+        document.getElementById("output-screen").value=operationValue;
+        operation=0;
+        operationValue=0;
+    }
+    else if(operation==3) {
+        operationValue=operationValue*parseInt(document.getElementById("output-screen").value);
+        document.getElementById("output-screen").value=operationValue;
+        operation=0;
+        operationValue=0;
+    }
+    else if(operation==4) {
+        operationValue=operationValue/parseInt(document.getElementById("output-screen").value);
+        if(parseInt(document.getElementById("output-screen").value)==0) {
+            document.getElementById("output-screen").value= "∞      " ;
+        }
+        else {
+        document.getElementById("output-screen").value=operationValue;
+        }
+        operation=0;
+        operationValue=0;
+    }
 }
